85661be22e65e9e33f07f12b9b2b0bd8a7f9153c
Matheus Silva <41930738+Matheuzera@users.noreply.github.com>
2023-06-07 13:59:46 -0300

    coments

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Win32;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,26 +12,26 @@ namespace Aula03Variaveis
         static void Main(string[] args)
         {
 
-            string c = "Carlos"; //string variavel texto (colocar um nome e em seguida colocar "")
-            //Juntar um texto com outro se chama CONCATENAÇÃO
-            int a = 18;
-            float altura = 1.70f; //colocar f senão não ira reconhecer
+            string c = "Vascão"; // string precisa estar sempre entre aspas
+            int a = 70;
+            float altura = 1.70f; //colocar f para o float ser considerado, senão da erro
             float money = 50.49f;
-            int soma = 78 + 192;//é possivel fazer soma na variavel
+            int soma = 78 + 192; //é possivel fazer soma na variavel
             float peso = 70.0f;
             float imc = peso / (altura * altura);
 
             Console.WriteLine("Aula 03 variaveis");
-            Console.WriteLine("Programa criado por " + c); //escrever variavel dentro do () dps da ""
+            Console.WriteLine("Programa criado por " + c); //variável fora das aspas
             Console.WriteLine("Eu sou o " + c);
             Console.WriteLine("Lembrando quem fez foi o cadu ");
             Console.WriteLine("minha idade é " + a);
             Console.WriteLine($"minha altura é {altura}");
-            Console.WriteLine("Meu nome é " + c + " e eu tenho " + a + "anos"); // colocar + e abrir "" para continuar escrevendo apos variavel
+            Console.WriteLine("Meu nome é " + c + " e eu tenho " + a + "anos"); // colocar + e abrir "" para continuar escrevendo apos variavel // concatenação
             Console.WriteLine($"Meu nome é {c} e eu tenho {a} anos");
             Console.WriteLine($"Na minha carteira tem {money}"); //($"{}" usar para escrita de variaveis em writeline sem a necessidade de usar + //interpolação
-            Console.WriteLine($"{imc}");
             Console.WriteLine($"{soma}");
+            Console.WriteLine($"{imc}");
+
 
             Console.ReadKey();
         }
